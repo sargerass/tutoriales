b2ce44c0cd125a5cda10981df0646453cb426416
sargerass <gdavidas3@gmail.com>
2023-02-16 06:12:54 -0500

    feat(add structure initial for javascript): :art: create structure of directories for javascript

---
+++ javascript/arrays/01-array-map.js
@@ -0,0 +1,29 @@
+const persons = [
+    { cars: 1, motorcycles: 1, name: "Russell" },
+    { cars: 0, motorcycles: 2, name: "Nadine" },
+    { cars: 4, motorcycles: 1, name: "Kenneth" },
+    { cars: 1, motorcycles: 0, name: "Teresa" },
+    { cars: 0, motorcycles: 0, name: "Patricia" },
+];
+console.log('person', persons);
+const INSURE_MOTORCYCLE = 500;
+const INSURE_CAR = 60;
+const MIN_AGE_ADULT = 18;
+
+// map
+
+
+// filter
+
+
+// find
+
+
+// reduce
+
+
+// some
+
+
+// sort
+
